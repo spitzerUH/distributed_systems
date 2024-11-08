60a43fdbeb4c42cf4d6e9ef90e0f45ac2fafb68e
Sami Saada <sami.saada@gmail.com>
2024-11-08 02:48:01 +0200

    Create peer connection and send offer

--- clients/main.js
+++ clients/main.js
@@ -1,15 +1,41 @@
 const SIGNALING_SERVER_URL = 'http://localhost:9999';
+const PC_CONFIG = {};
 
 let socket = io(SIGNALING_SERVER_URL, { autoConnect: false });
 
+let pc1;
+
 socket.on('newclient', () => {
-    console.log('New client just joined');
+    console.info('New client just joined');
+    pc1 = createPeerConnection();
+    sendOffer(pc1);
 });
 
 socket.on('data', (data) => {
-    console.log('Data received: ', data);
+    console.info('Data received: ', data);
 });
 
+let createPeerConnection = () => {
+    console.debug('createPeerConnection');
+    let pc;
+    try {
+        pc = new RTCPeerConnection(PC_CONFIG);
+        return pc;
+    } catch(error) {
+        console.error('createPeerConnection');
+    }
+};
+
+let sendOffer = (pc) => {
+    console.debug('sendOffer');
+    pc.createOffer().then((sessionDescription) => {
+        console.log(sessionDescription);
+        pc.setLocalDescription(sessionDescription);
+        socket.emit('data', sessionDescription);
+    }, (error) => {
+        console.error(error);
+    });
+};
 
 // For now just connect
 socket.connect();
