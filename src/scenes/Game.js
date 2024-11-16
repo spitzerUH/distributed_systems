a45e71e02d7e33a21d3cabefb0a9363189f55f4b
Sami Saada <sami.saada@gmail.com>
2024-11-17 00:33:34 +0200

    Create detailed game information with room name and latest cursor direction.

--- src/scenes/Game.js
+++ src/scenes/Game.js
@@ -13,7 +13,42 @@ export class Game extends Scene
 
     create ()
     {
+
+        this.cursors = this.input.keyboard.createCursorKeys();
+
         this.cameras.main.setBackgroundColor(0x00ff00);
-        this.rexUI.add.BBCodeText(0, 0, `Room: ${this.connection.room}`, {color: '#000'});
+        var background = this.add.rectangle(0, 0, 10, 10, 0x000000);
+        this.roominfo = this.rexUI.add.BBCodeText(0, 0, `Room: ${this.connection.room}`, {color: '#fff'});
+        this.direction = this.add.text(0,0, 'direction');
+        var viewport = this.rexUI.viewport;
+        this.gameinfo = this.rexUI.add.sizer({
+            x: viewport.centerX,
+            y: 10,
+            orientation: 'x',
+        })
+            .addBackground(background)
+            .add(this.roominfo, 0, 'left', {left: 10, right: 10, top: 5, bottom: 5}, true)
+            .add(this.direction, 0, 'left', {left: 10, right: 10, top: 5, bottom: 5}, true)
+            .layout();
+    }
+
+    update (time, delta)
+    {
+        if (this.cursors.left.isDown)
+        {
+            this.direction.setText('left');
+        }
+        else if (this.cursors.right.isDown)
+        {
+            this.direction.setText('right');
+        }
+        else if (this.cursors.up.isDown)
+        {
+            this.direction.setText('up');
+        }
+        else if (this.cursors.down.isDown)
+        {
+            this.direction.setText('down');
+        }
     }
 }
