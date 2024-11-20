7638e8f022f2db2bb524fc1e433cfe41069cf696
Christoph Spitzer <christoph.spitzer@student.tugraz.at>
2024-11-20 13:12:25 +0200

    changed camera settings and background to gradient

--- src/scenes/Game.js
+++ src/scenes/Game.js
@@ -11,98 +11,195 @@ export class Game extends Scene
         this.connection = data.connection;
     }
 
-    create ()
-    {
+  create() {
+    this.cursors = this.input.keyboard.createCursorKeys();
 
-        this.cursors = this.input.keyboard.createCursorKeys();
+    var viewport = this.rexUI.viewport;
 
-        this.cameras.main.setBackgroundColor(0x00ff00);
-        var background = this.add.rectangle(0, 0, 10, 10, 0x000000);
-        this.roominfo = this.rexUI.add.BBCodeText(0, 0, `Room: ${this.connection.room}`, {color: '#fff'});
-        this.direction = this.add.text(0,0, 'direction');
-        var viewport = this.rexUI.viewport;
-        this.gameinfo = this.rexUI.add.sizer({
-            x: viewport.centerX,
-            y: 10,
-            orientation: 'x',
-        })
-            .addBackground(background)
-            .add(this.roominfo, 0, 'left', {left: 10, right: 10, top: 5, bottom: 5}, true)
-            .add(this.direction, 0, 'left', {left: 10, right: 10, top: 5, bottom: 5}, true)
-            .layout();
-        this.dirr = undefined;
+    this.player = this.add.circle(
+      viewport.centerX,
+      viewport.centerY,
+      10,
+      0x000000
+    );
+
+    this.physics.add.existing(this.player);
+
+    this.mainCamera = this.initMainCamera();
+    this.miniMapCamera = this.initMiniMap();
+
+    this.addDebugTextField();
+
+    this.dirr = undefined;
+    this.dirrSending = false;
+
+    this.players = {};
+
+    this.connection.openDataChannel = (playerid) => {
+      let otherplayer = this.add.circle(
+        viewport.centerX,
+        viewport.centerY,
+        10,
+        0x000000
+      );
+      this.physics.add.existing(otherplayer);
+      this.players[playerid] = otherplayer;
+    };
+
+    this.connection.receivedMessage = (playerid, message) => {
+      console.log(message);
+      let command = JSON.parse(message);
+      switch (command.moving) {
+        case "left":
+          this.players[playerid].body.setVelocity(-100, 0);
+          break;
+        case "right":
+          this.players[playerid].body.setVelocity(100, 0);
+          break;
+        case "up":
+          this.players[playerid].body.setVelocity(0, -100);
+          break;
+        case "down":
+          this.players[playerid].body.setVelocity(0, 100);
+          break;
+      }
+    };
+  }
+
+  update(time, delta) {
+    var curDirr = undefined;
+    if (this.cursors.left.isDown) {
+      this.direction?.setText("left");
+      curDirr = "left";
+      this.player.body.setVelocity(-100, 0);
+    } else if (this.cursors.right.isDown) {
+      this.direction?.setText("right");
+      curDirr = "right";
+      this.player.body.setVelocity(100, 0);
+    } else if (this.cursors.up.isDown) {
+      this.direction?.setText("up");
+      curDirr = "up";
+      this.player.body.setVelocity(0, -100);
+    } else if (this.cursors.down.isDown) {
+      this.direction?.setText("down");
+      curDirr = "down";
+      this.player.body.setVelocity(0, 100);
+    }
+    if (curDirr && curDirr != this.dirr) {
+      this.sendMovement(curDirr);
+    }
+
+    this.gameinfo?.setPosition(
+      this.cameras.main.worldView.x + Math.floor(this.gameinfo.width / 2),
+      this.cameras.main.worldView.y + Math.floor(this.gameinfo.height / 2)
+    );
+  }
+
+  sendMovement(movement) {
+    if (!this.dirrSending) {
+      this.dirrSending = true;
+      this.connection.sendMessage(`{"moving": "${movement}"}`).then(() => {
+        this.dirr = movement;
         this.dirrSending = false;
-
-        this.player = this.add.circle(viewport.centerX, viewport.centerY, 10, 0x000000);
-        this.physics.add.existing(this.player);
-
-        this.players = {};
-
-        this.connection.openDataChannel = (playerid) => {
-            let otherplayer = this.add.circle(viewport.centerX, viewport.centerY, 10, 0x000000);
-            this.physics.add.existing(otherplayer);
-            this.players[playerid] = otherplayer;
-        }
-
-        this.connection.receivedMessage = (playerid, message) => {
-            console.log(message);
-            let command = JSON.parse(message);
-            switch (command.moving) {
-                case 'left':
-                    this.players[playerid].body.setVelocity(-100,0);
-                    break;
-                case 'right':
-                    this.players[playerid].body.setVelocity(100,0);
-                    break;
-                case 'up':
-                    this.players[playerid].body.setVelocity(0,-100);
-                    break;
-                case 'down':
-                    this.players[playerid].body.setVelocity(0,100);
-                    break;
-            }
-        };
+      });
     }
+  }
 
-    update (time, delta)
-    {
-        var curDirr = undefined;
-        if (this.cursors.left.isDown)
-        {
-            this.direction.setText('left');
-            curDirr = 'left';
-            this.player.body.setVelocity(-100,0);
-        }
-        else if (this.cursors.right.isDown)
-        {
-            this.direction.setText('right');
-            curDirr = 'right';
-            this.player.body.setVelocity(100,0);
-        }
-        else if (this.cursors.up.isDown)
-        {
-            this.direction.setText('up');
-            curDirr = 'up';
-            this.player.body.setVelocity(0,-100);
-        }
-        else if (this.cursors.down.isDown)
-        {
-            this.direction.setText('down');
-            curDirr = 'down';
-            this.player.body.setVelocity(0,100);
-        }
-        if (curDirr && curDirr != this.dirr) {
-            this.sendMovement(curDirr);
-        }
-    }
+  initMainCamera() {
+    const width = this.cameras.main.width;
+    const height = this.cameras.main.height;
+    const gradientTexture = this.textures.createCanvas(
+      "gradientBackground",
+      width,
+      height
+    );
 
-    sendMovement(movement) {
-        if (!this.dirrSending) {
-            this.dirrSending = true;
-            this.connection.sendMessage(`{"moving": "${movement}"}`).then(() => {
-                this.dirr = movement;
-                this.dirrSending = false;
-            });
-        }
-    }
+    const ctx = gradientTexture.getSourceImage().getContext("2d");
+
+    const gradient = ctx.createRadialGradient(
+      width / 2,
+      height / 2,
+      0,
+      width / 2,
+      height / 2,
+      Math.max(width, height)
+    );
+
+    gradient.addColorStop(0, "#00ff00");
+    gradient.addColorStop(1, "#002200");
+
+    ctx.fillStyle = gradient;
+    ctx.fillRect(0, 0, width, height);
+
+    gradientTexture.refresh();
+
+    this.add.image(0, 0, "gradientBackground").setOrigin(0).setDepth(-2);
+
+    const mainCamera = this.cameras.main;
+
+    mainCamera.setBounds(0, 0, width, height);
+    mainCamera.startFollow(this.player);
+    mainCamera.setZoom(3);
+
+    return mainCamera;
+  }
+
+  initMiniMap() {
+    const miniMapFactor = 0.2;
+    const width = this.cameras.main.width;
+    const height = this.cameras.main.height;
+    const miniMapWidth = width * miniMapFactor;
+    const miniMapHeight = height * miniMapFactor;
+
+    const miniMapCamera = this.cameras.add(
+      width - miniMapWidth,
+      height - miniMapHeight,
+      width,
+      height,
+      false,
+      "miniMap"
+    );
+    miniMapCamera.setZoom(miniMapFactor);
+    miniMapCamera.setBounds(0, 0, width, height);
+
+    return miniMapCamera;
+  }
+
+  addDebugTextField() {
+    var background = this.add.rectangle(0, 0, 10, 10, 0x000000);
+
+    this.roominfo = this.rexUI.add.BBCodeText(
+      0,
+      0,
+      `Room: ${this.connection.room}`,
+      { color: "#fff", fontSize: "12px" }
+    );
+    this.direction = this.add.text(0, 0, "direction", { fontSize: "12px" });
+
+    this.gameinfo = this.rexUI.add
+      .sizer({
+        x: 0,
+        y: 10,
+        orientation: "x",
+      })
+      .addBackground(background)
+      .add(
+        this.roominfo,
+        0,
+        "left",
+        { left: 10, right: 10, top: 5, bottom: 5 },
+        true
+      )
+      .add(
+        this.direction,
+        0,
+        "left",
+        { left: 10, right: 10, top: 5, bottom: 5 },
+        true
+      )
+      .layout();
+
+    this.gameinfo.setDepth(100);
+    this.miniMapCamera.ignore(this.gameinfo);
+  }
 }
