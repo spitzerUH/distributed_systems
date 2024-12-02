3e5d48a467316403af1610038837ec8746acaf97
Sami Saada <sami.saada@gmail.com>
2024-12-02 18:05:35 +0200

    Refactor Player class to accept initialization data and create object in scene

--- src/objects/player.js
+++ src/objects/player.js
@@ -1,16 +1,26 @@
 class Player {
-  constructor() {
-    this._id = undefined;
-    this._name = undefined;
-    this._color = undefined;
-    this._observing = undefined;
-    this._spawnpoint = { x: 0, y: 0 };
-    this._status = undefined;
+  constructor(data) {
+    this._id = data.id;
+    this._name = data.name;
+    this._color = data.color;
+    this._observing = data.observing;
+    this._position = data.position;
+    this._status = data.status;
     this._object = undefined;
+  }
+  createObject(scene) {
+    this.object = scene.add.circle(
+      this._position.x,
+      this._position.y,
+      10,
+      this._color
+    );
+    scene.physics.add.existing(this.object);
   }
 }
 
-export function createPlayer(data) {
-  let player = new Player();
+export function createPlayer(scene, data) {
+  let player = new Player(data);
+  player.createObject(scene);
   return player;
 }
