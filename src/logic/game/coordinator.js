96c3d8775bace1f4f98919d75858dec27ed6c394
Sami Saada <sami.saada@gmail.com>
2024-12-03 14:33:32 +0200

    Refactor joinRoom and observe methods to return promises for better error handling

--- src/logic/game/coordinator.js
+++ src/logic/game/coordinator.js
@@ -11,13 +11,25 @@ class Coordinator {
   }
 
   joinRoom(roomCode) {
-    this._gameState = new GameState({ connection: this._connectionManager, observer: false });
-    return this._connectionManager.joinRoom(roomCode);
+    return new Promise((resolve, reject) => {
+      this._gameState = new GameState({ connection: this._connectionManager, observer: false });
+      this._connectionManager.joinRoom(roomCode).then(() => {
+        resolve();
+      }).catch((err) => {
+        reject(err);
+      });
+    });
   }
 
   observe(roomCode) {
-    this._gameState = new GameState({ connection: this._connectionManager, observer: true });
-    return this._connectionManager.joinRoom(roomCode);
+    return new Promise((resolve, reject) => {
+      this._gameState = new GameState({ connection: this._connectionManager, observer: true });
+      this._connectionManager.joinRoom(roomCode).then(() => {
+        resolve();
+      }).catch((err) => {
+        reject(err);
+      });
+    });
   }
 }
 
