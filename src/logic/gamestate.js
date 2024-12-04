ec8d3c8d4c584c902b2305dc64d73edf15dd67a2
Sami Saada <sami.saada@gmail.com>
2024-12-04 17:18:41 +0200

    Refactor GameState class: remove inheritance from EventEmitter

--- src/logic/gamestate.js
+++ src/logic/gamestate.js
@@ -1,9 +1,7 @@
-import { EventEmitter } from 'events';
 import { createPlayer } from '+objects/player';
 
-export class GameState extends EventEmitter {
+export class GameState {
   constructor(data = {}) {
-    super();
     this._connection = data.connection;
     this._players = {};
     this._food = {};
