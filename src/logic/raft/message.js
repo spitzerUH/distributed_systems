0f345c481f64eaa1484b168235e4b179d3575bf9
Christoph Spitzer <christoph.spitzer@student.tugraz.at>
2024-12-05 18:31:37 +0200

    #81 - updated README.md and moved raft messages to another file

---
+++ src/logic/raft/message.js
@@ -0,0 +1,46 @@
+class Message {
+  constructor(type) {
+    this._type = type;
+  }
+  doAction(state, emitter) {
+    return new Promise((resolve, reject) => {
+      reject('doAction not implemented');
+    });
+  }
+}
+
+function formatRaftElectionRequest(term, uuid) {
+  return {
+    platform: 'raft',
+    type: 'raft-election-request',
+    data: {
+      requestFrom: uuid,
+      term: term
+    }
+  };
+}
+
+function formatRaftElectionVote(term, uuid) {
+  return {
+    platform: 'raft',
+    type: 'raft-election-vote',
+    data: {
+      voteFor: uuid,
+      term: term
+    }
+  };
+}
+
+function formatRaftElectionLeader(term, uuid) {
+  return {
+    platform: 'raft',
+    type: 'raft-election-leader',
+    data: {
+      currentLeader: uuid,
+      term: term
+    }
+  };
+}
+
+
+export { formatRaftElectionRequest, formatRaftElectionVote, formatRaftElectionLeader };
